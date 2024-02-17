7306a7a00071ccbcf83cba51b6f6ea0650f161ff
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-02-17 15:33:26 +0100

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -4,3 +4,5 @@ function notify(){
   message=`echo -ne "##Kraken:\n *$1*" | sed 's/[^a-zA-Z 0-9*_]/\\\\&/g'`
   curl -s -X POST "https://api.telegram.org/bot$telegram_api_key/sendMessage" -d chat_id="$telegram_chat_id" -d text="$message" -d parse_mode="MarkdownV2" &> /dev/null
 }
+
+notify "Esto es una prueba"
