1fddd70660ed421e4fccf3791092cf59a75dfca6
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-01 12:32:14 +0100

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+domains_file="/root/tools/domains.txt"
+
+if [ ! -f "$domains_file" ]; then
+    echo "El archivo de dominios $domains_file no existe."
+    exit 1
+fi
+
+while IFS= read -r domain; do
+    bash kraken.sh -domain "$domain" -recon -wayback -nuclei_cves -nuclei_dlogins -nuclei_panels -nuclei_exposures -nuclei_misc -nuclei_misconfig -nuclei_takeovers -nuclei_tech -nuclei_vuln -cors -nmap -crlf -or -output
+    sleep 5
+done < "$domains_file"
