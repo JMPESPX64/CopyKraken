0dc9a0a5119c63762ba3ee86c40f335b98573a63
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-01 16:22:35 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -9,5 +9,6 @@ fi
 
 while IFS= read -r domain; do
     bash ~/tools/ElKraken/ElKrakenTool.sh -domain "$domain" -recon -wayback -nuclei_cves -nuclei_dlogins -nuclei_panels -nuclei_exposures -nuclei_misc -nuclei_misconfig -nuclei_takeovers -nuclei_tech -nuclei_vuln -cors -nmap -crlf -or -output
+    wait
     sleep 5
 done < "$domains_file"
