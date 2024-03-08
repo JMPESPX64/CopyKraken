ab5b3bdf5a12f702eacc9fd386eef06955c42da6
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-08 19:00:19 +0100

    Update install.sh

--- Tools/install.sh
+++ Tools/install.sh
@@ -21,9 +21,110 @@ sudo apt install -y tmux
 sudo apt install -y dnsutils
 sudo apt install -y curl
 sudo apt install -y nmap
+sudo apt install -y dos2unix
+pip3 install dirsearch
 
-export GO111MODULE=on
-go get github.com/projectdiscovery/katana/cmd/katana@latest
+pip install colored
+pip3 install colored
+pip3 install uro
+pip3 install --break-system-packages uro
+pip3 install requests
 
+mkdir /root/tools
+
+git clone https://github.com/projectdiscovery/nuclei-templates
+
+git clone https://github.com/rockysec/customscripts /root/tools/customscripts
+
+git clone https://github.com/udhos/update-golang /root/tools/update-golang
+sudo bash /root/tools/update-golang/update-golang.sh
+
+git clone https://github.com/aboul3la/Sublist3r.git /root/tools/Sublist3r
+cd /root/tools/Sublist3r*
+pip3 install -r requirements.txt
+
+git clone https://github.com/maurosoria/dirsearch.git /root/tools/dirsearch
+
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git /root/tools/sqlmap
+
+git clone https://github.com/s0md3v/Corsy /root/tools/Corsy
+
+git clone https://github.com/danielmiessler/SecLists.git /root/tools/SecLists
+
+cd /root/tools/SecLists/Discovery/DNS/
+cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
+
+# Install httpx
+curl -L -O https://github.com/projectdiscovery/httpx/releases/download/v1.0.3/httpx_1.0.3_linux_amd64.tar.gz
+tar -xzvf httpx_1.0.3_linux_amd64.tar.gz
+mv httpx /usr/local/bin/
+
+# Install crlfuzz
+curl -sSfL https://git.io/crlfuzz | sh -s -- -b /usr/local/bin
+
+# Install subfinder
+cd /root/tools
+latest_version=$(curl -s https://api.github.com/repos/projectdiscovery/subfinder/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link="https://github.com/projectdiscovery/subfinder/releases/download/${latest_version}/subfinder_$(echo $latest_version | tr -d 'v')_linux_amd64.zip"
+curl -LO $download_link
+unzip subfinder_$(echo $latest_version | tr -d 'v')_linux_amd64.zip
+cp subfinder /usr/bin/subfinder
+
+# Install nuclei
+cd /root/tools/
+curl -L -O https://github.com/projectdiscovery/nuclei/releases/download/v3.1.10/nuclei_3.1.10_linux_amd64.zip
+unzip nuclei_3.1.10_linux_amd64.zip
+mv nuclei /usr/bin/
+git clone https://github.com/projectdiscovery/nuclei-templates
+nuclei -update
+
+# Install amass
+cd /root/tools
+latest_version=$(curl -s https://api.github.com/repos/owasp-amass/amass/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link="https://github.com/owasp-amass/amass/releases/download/${latest_version}/amass_Linux_amd64.zip"
+curl -LO $download_link
+unzip -o amass_Linux_amd64.zip
+cp amass_Linux_amd64/amass /usr/local/bin/amass
+
+# Install dalfox
+cd /root/tools
+latest_version=$(curl -s https://api.github.com/repos/hahwul/dalfox/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link="https://github.com/hahwul/dalfox/releases/download/${latest_version}/dalfox_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz"
+curl -LO $download_link
+tar -xf dalfox_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz
+mv dalfox /usr/local/bin/dalfox
+
+# Install GAU
+cd /root/tools
+latest_version=$(curl -s https://api.github.com/repos/lc/gau/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link="https://github.com/lc/gau/releases/download/${latest_version}/gau_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz"
+curl -LO $download_link
+tar -xf gau_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz
+cp gau /usr/bin/gau
+
+# Install Katana
+cd /root/tools
+latest_version=$(curl -s https://api.github.com/repos/projectdiscovery/katana/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link="https://github.com/projectdiscovery/katana/releases/download/${latest_version}/katana_$(echo $latest_version | tr -d 'v')_linux_amd64.zip"
+curl -LO $download_link
+unzip -o katana_$(echo $latest_version | tr -d 'v')_linux_amd64.zip
+cp katana /usr/bin/katana
+
+# Install Gf-Patterns
+cd /root
+git clone https://github.com/1ndianl33t/Gf-Patterns /root/Gf-Patterns
+mkdir .gf
+mv /root/Gf-Patterns/*.json /root/.gf
+
+GO111MODULE=on go get github.com/tomnomnom/anew@latest
+GO111MODULE=on go get github.com/tomnomnom/gf@latest
+GO111MODULE=on go get github.com/tomnomnom/qsreplace@latest
+GO111MODULE=on go get github.com/tomnomnom/httpx@latest
+GO111MODULE=on go get github.com/tomnomnom/waybackurls@latest
+GO111MODULE=on go get github.com/KathanP19/Gxss@latest
+GO111MODULE=on go get github.com/Emoe/kxss@latest
+GO111MODULE=on go get github.com/tomnomnom/assetfinder@latest
+nuclei -update-templates
+sudo cp /root/go/bin/* /usr/bin
 
 
