2970292db42c7d6f08b4b7032397778b0b56d812
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-08 19:25:42 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-domains_file="/root/tools/domains.txt"
-
-if [ ! -f "$domains_file" ]; then
-    echo "El archivo de dominios $domains_file no existe."
-    exit 1
-fi
-
-while IFS= read -r domain; do
-    bash ~/tools/ElKraken/ElKrakenTool.sh -domain "$domain" -recon -wayback -nuclei_cves -nuclei_dlogins -nuclei_panels -nuclei_exposures -nuclei_misc -nuclei_misconfig -nuclei_takeovers -nuclei_tech -nuclei_vuln -cors -nmap -crlf -or -output
-    wait
-    sleep 5
-done < "$domains_file"
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
