10a0df487e2c9dbf03e1f1cea3d1e6823e3bc79c
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-27 15:26:55 +0100

    Update install.sh

--- Tools/install.sh
+++ Tools/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-sudo apt update
-sudo apt install -y libcurl4-openssl-dev tar
+apt update
+apt install moreutils -y
+apt install sudo -y
+sudo apt install -y libcurl4-openssl-dev tar zip unzip
 sudo apt install -y libssl-dev
 sudo apt install -y jq
 sudo apt install -y ruby-full
@@ -23,115 +24,46 @@ sudo apt install -y dnsutils
 sudo apt install -y curl
 sudo apt install -y nmap
 sudo apt install -y dos2unix
-pip3 install dirsearch --break-system-packages
 
-pip install colored --break-system-packages
-pip3 install colored --break-system-packages
 pip3 install uro --break-system-packages
-pip3 install requests --breadk-system-packages
+pip3 install requests --break-system-packages
 
-mkdir /root/tools
+# Aquatone
+mkdir -p /root/tools/aquatone_dir
+cd /root/tools/aquatone_dir
+latest_version_aquatone=$(curl -s https://api.github.com/repos/michenriksen/aquatone/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+download_link_aquatone="https://github.com/michenriksen/aquatone/releases/download/${latest_version_aquatone}/aquatone_linux_amd64_$(echo $latest_version_aquatone | tr -d 'v').zip"
+curl -LO $download_link_aquatone
+unzip /root/tools/aquatone_linux_amd64_$(echo $latest_version | tr -d 'v').zip
+cp /root/tools/aquatone_dir/aquatone /usr/local/bin/aquatone
 
-git clone https://github.com/projectdiscovery/nuclei-templates
-
-git clone https://github.com/rockysec/customscripts /root/tools/customscripts
-
-git clone https://github.com/udhos/update-golang /root/tools/update-golang
-sudo bash /root/tools/update-golang/update-golang.sh
-
-git clone https://github.com/aboul3la/Sublist3r.git /root/tools/Sublist3r
-cd /root/tools/Sublist3r*
-pip3 install -r requirements.txt
-
-git clone https://github.com/maurosoria/dirsearch.git /root/tools/dirsearch
-
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git /root/tools/sqlmap
-
-git clone https://github.com/s0md3v/Corsy /root/tools/Corsy
-
-git clone https://github.com/danielmiessler/SecLists.git /root/tools/SecLists
-
-cd /root/tools/SecLists/Discovery/DNS/
-cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-
-# Install httpx
-curl -L -O https://github.com/projectdiscovery/httpx/releases/download/v1.0.3/httpx_1.0.3_linux_amd64.tar.gz
-tar -xzvf httpx_1.0.3_linux_amd64.tar.gz
-mv httpx /usr/local/bin/
+# Install SecretFinder
+git clone https://github.com/m4ll0k/SecretFinder.git /root/tools/secretfinder
+cd /root/tools/secretfinder
+pip3 install -r requirements.txt --break-system-packages
 
 # Install crlfuzz
 curl -sSfL https://git.io/crlfuzz | sh -s -- -b /usr/local/bin
 
-# Install subfinder
-cd /root/tools
-latest_version=$(curl -s https://api.github.com/repos/projectdiscovery/subfinder/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-download_link="https://github.com/projectdiscovery/subfinder/releases/download/${latest_version}/subfinder_$(echo $latest_version | tr -d 'v')_linux_amd64.zip"
-curl -LO $download_link
-unzip subfinder_$(echo $latest_version | tr -d 'v')_linux_amd64.zip
-cp subfinder /usr/bin/subfinder
+# Install corsy
+git clone https://github.com/s0md3v/Corsy /root/tools/Corsy
+pip3 install -r /root/tools/Corsy/requirements.txt --break-system-packages
 
-# Install nuclei
-cd /root/tools/
-curl -L -O https://github.com/projectdiscovery/nuclei/releases/download/v3.1.10/nuclei_3.1.10_linux_amd64.zip
-unzip nuclei_3.1.10_linux_amd64.zip
-mv nuclei /usr/bin/
-git clone https://github.com/projectdiscovery/nuclei-templates
-nuclei -update
-
-# Install amass
-cd /root/tools
-latest_version=$(curl -s https://api.github.com/repos/owasp-amass/amass/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-download_link="https://github.com/owasp-amass/amass/releases/download/${latest_version}/amass_Linux_amd64.zip"
-curl -LO $download_link
-unzip -o amass_Linux_amd64.zip
-cp amass_Linux_amd64/amass /usr/local/bin/amass
-
-# Install dalfox
-cd /root/tools
-latest_version=$(curl -s https://api.github.com/repos/hahwul/dalfox/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-download_link="https://github.com/hahwul/dalfox/releases/download/${latest_version}/dalfox_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz"
-curl -LO $download_link
-tar -xf dalfox_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz
-mv dalfox /usr/local/bin/dalfox
-
-# Install GAU
-cd /root/tools
-latest_version=$(curl -s https://api.github.com/repos/lc/gau/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-download_link="https://github.com/lc/gau/releases/download/${latest_version}/gau_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz"
-curl -LO $download_link
-tar -xf gau_$(echo $latest_version | tr -d 'v')_linux_amd64.tar.gz
-cp gau /usr/bin/gau
-
-# Install Katana
-cd /root/tools
-latest_version=$(curl -s https://api.github.com/repos/projectdiscovery/katana/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-download_link="https://github.com/projectdiscovery/katana/releases/download/${latest_version}/katana_$(echo $latest_version | tr -d 'v')_linux_amd64.zip"
-curl -LO $download_link
-unzip -o katana_$(echo $latest_version | tr -d 'v')_linux_amd64.zip
-cp katana /usr/bin/katana
-
-# Install secretfinder
-cd /root/tools
-git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
-cd /root/tools/secretfinder
-pip3 install -r requirements.txt --break-system-packages
-cp SecretFinder.py /usr/bin/secretfinder.py
-
-# Install Gf-Patterns
-cd /root
-git clone https://github.com/1ndianl33t/Gf-Patterns /root/Gf-Patterns
-mkdir .gf
+# GF-PATTERNS
+mkdir /root/.gf
+git clone https://github.com/1ndianl33t/Gf-Patterns /root/tools/Gf-Patterns
 mv /root/Gf-Patterns/*.json /root/.gf
 
+
+# Go packages
+go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+go install github.com/Emoe/kxss@latest
 go install -v github.com/tomnomnom/anew@latest
-go install github.com/tomnomnom/gf@latest
-go install github.com/tomnomnom/qsreplace@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/tomnomnom/waybackurls@latest
-go install github.com/KathanP19/Gxss@latest
+go install github.com/tomnomnom/gf@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install github.com/tomnomnom/assetfinder@latest
-go install github.com/Emoe/kxss@latest
-nuclei -update-templates
-sudo cp /root/go/bin/* /usr/bin
-
 
