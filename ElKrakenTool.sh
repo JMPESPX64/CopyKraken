c5494ae459234f5eac676c4bfacfa829339cd4f5
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-27 15:27:32 +0100

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -1,82 +1,15 @@
 #!/bin/bash
-############################################
-#                                          #
-#     Your APITokens and Variables here    #
-#                                          #
-############################################
-dirsearchExtensions="sql,txt,zip,jsp,log,logs,old,tar.gz,gz,tar,tgz,bkp,dump,db,php,php3,php4,php5,,xml,py,asp,aspx,rar,do,1,asmx,rar,key,gpg,asc,pl,js,shtm,shtml,phtm,phtml,jhtml,cfm,cfml,rb,cfg,pdf,doc,docx,xls,xlsx,conf"
-tokenSlack="YOUR_TOKEN"
-channelSlack="YOUR_CHANNEL"
-directory_tools=~/tools
-directory_data=/root
-ssh_conection="user@ipadd:/folder" ##reemplazar user, ipaddr y folder por los datos de tu servidor repositorio de resultados de escaneo
-notify=true
-########################################
 
-
-function logo {
-echo " _____ _       _  ______      _    _  _______ _   _ "
-echo "| ____| |     | |/ /  _ \    / \  | |/ / ____| \ | |"
-echo "|  _| | |     | ' /| |_) |  / _ \ | ' /|  _| |  \| |"
-echo "| |___| |___  | . \|  _ <  / ___ \| . \| |___| |\  |"
-echo "|_____|_____| |_|\_\_| \_\/_/   \_\_|\_\_____|_| \_|"
-echo ""
-}
-
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-reset=`tput sgr0`
-SECONDS=0
-domain=$2
-#subreport=
-#usage() { echo -e "Usage: $0 -d domain [-e]\n  Select -e to specify excluded domains\n " 1>&2; exit 1; }
-#while getopts ":d:e:r:" o; do
-#    case "${o}" in
-#        d)
-#            domain=${OPTARG}
-#            ;;
-
-            #### working on subdomain exclusion
-#        e)
-#            excluded=${OPTARG}
-#            ;;
-#                r)
-#            subreport+=("$OPTARG")
-#            ;;
-#        *)
-#            usage
-#            ;;
-#    esac
-#done
-
-#shift $((OPTIND - 1))
-#if [ -z "${domain}" ] && [[ -z ${subreport[@]} ]]; then
-#   usage; exit 1;
-#fi
-
-function flags {
-  echo "${yellow}Argumentos permitidos:"
-  echo "-domain <argumento>: Realiza la tarea 1 con el argumento especificado"
-  echo "-recon: Realiza validacion dns, urls, etc"
-  echo "-wayback: Realiza recopilacion de info en wayback url"
-  echo "-dirsearch: Realiza fuzzing de directorios"
-  echo "-nuclei_cves: Realiza scaneos con nuclei en busca de vulnerabilidades"
-  echo "-nuclei_dlogins: Realiza scaneos con nuclei en busca de Default Logins"
-  echo "-nuclei_panels: Realiza scaneos con nuclei en busca de panels de login"
-  echo "-nuclei_exposures: Realiza scaneos con nuclei en busca de informacion expuesta"
-  echo "-nuclei_misc: Realiza scaneos con nuclei en busca de misc"
-  echo "-nuclei_misconfig: Realiza scaneos con nuclei en busca de misconfiguration"
-  echo "-nuclei_takeovers: Realiza scaneos con nuclei en busca de posibles dns takeover"
-  echo "-nuclei_tech: Realiza scaneos con nuclei en busca de deteccion de tecnologias usadas"
-  echo "-nuclei_vuln: Realiza scaneos con nuclei en busca de vulnerabilidades varias"
-  echo "-cors: Analiza si las url son vulnerables a Cors"
-  echo "-nmap: Realiza scan a todos los puertos de manera agresiva en todos los subdominios"
-  echo "-crlf: Realiza busqueda de vulnerabilidad CRLF"
-  echo "-or: Realiza la busqueda de Open Redirecg"
-  echo "-output: Envia la data recopilada al nodo de ELK"
-
-}
+notify=true 
+directory_data=/root 
+mkdir $directory_data/tools
+tools_dir=$directory_data/tools
+mkdir -p $directory_data/$domain/subdomains
+mkdir $directory_data/$domain/httpx_info
+mkdir $directory_data/$domain/screenshots
+mkdir $directory_data/$domain/vulns
+mkdir $directory_data/$domain/wayback_urls
+mkdir $directory_data/$domain/secrets
 
 function notify {
     if [ "$notify" = true ]
@@ -100,267 +33,53 @@ function notify {
     fi
 }
 
-# Verificar si se pasaron argumentos
-if [ $# -eq 0 ]; then
-  echo "${red}Debe pasar al menos un argumento."
-  flags
-  exit 1
-fi
+# Find subdomains 
+notify "Listing subdomains"
+chaos -dL $1 -silent >> $directory_data/$domain/subdomains/subdomains.txt
+subfinder -dL $1 -all -silent | anew $directory_data/$domain/subdomains/subdomains.txt
+assetfinder --subs-only $1 | anew $directory_data/$domain/subdomains/subdomains.txt 
 
-# Variables de control
-domain=""
-recon=false
-wayback=false
-dirsearch=false
-nuclei_cves=false
-nuclei_dlogins=false
-nuclei_panels=false
-nuclei_exposures=false
-nuclei_misc=false
-nuclei_misconfig=false
-nuclei_takeovers=false
-nuclei_tech=false
-nuclei_vuln=false
-cors=false
-nmap=false
-crlf=false
-or=false
-output=false
+# Get alive subdomains 
+notify "Running httpx"
+cat $directory_data/$domain/subdomains/subdomains.txt | httpx -title -tech-detect -status-code -ip -p 80,443,8080,8081,9002,8443,81 >> $directory_data/$domain/httpx_info/httpx_full_info.txt
 
-while [[ $# -gt 0 ]]; do
-    key="$1"
+cat $directory_data/$domain/httpx_info/httpx_full_info.txt | awk '{print $1}' >> $directory_data/$domain/httpx_info/alive_subdomains.txt
 
-    case $key in
+# Take screenshots
+notify "Taking screenshots"
+cat $directory_data/$domain/httpx_info/alive_subdomains.txt | aquatone -out $directory_data/$domain/screenshots
 
-   -domain)
-         if [ -z "$2" ]; then
-        echo "${red}Falta el argumento para -domain."
-        flags
-        exit 1
-      fi
+# Subdomains takeover 
+notify "Check subdomains takeover with subzy"
+subzy run --targets $directory_data/$domain/httpx_info/alive_subdomains.txt --hide_fails >> $directory_data/$domain/vulns/posible_takeover.txt
 
-      domain="$2"
-      shift 2
-      ;;
+# XSS
+notify "Running gau"
+gau --threads 16 --subs --blacklist png,jpg,gif,svg,woff,woff2 $1 >> $directory_data/$domain/wayback_urls/urls.txt
+waybackurls $1 | anew $directory_data/$domain/wayback_urls/urls.txt
+notify "Running katana"
+katana -list $1 -d 5 -jc -silent | anew $directory_data/$domain/secrets/katana.txt
+cat $directory_data/$domain/secrets/katana.txt >> $directory_data/$domain/wayback_urls/urls.txt
+gf xss $directory_data/$domain/wayback_urls/urls.txt >> $directory_data/$domain/wayback_urls/parameter_urls.txt
+cat $directory_data/$domain/wayback_urls/parameter_urls.txt | uro | kxss | grep -v "\[\]" >> $directory_data/$domain/vulns/posible_xss.txt
 
-    -recon)
-      recon=true
-      shift
-      ;;
-    -wayback)
-      wayback=true
-      shift
-      ;;
-    -dirsearch)
-      dirsearch=true
-      shift
-      ;;
-    -nuclei_cves)
-      nuclei_cves=true
-      shift
-      ;;
-    -nuclei_dlogins)
-      nuclei_dlogins=true
-      shift
-      ;;
-    -nuclei_panels)
-      nuclei_panels=true
-      shift
-      ;;
-    -nuclei_exposures)
-      nuclei_exposures=true
-      shift
-      ;;
-    -nuclei_misc)
-      nuclei_misc=true
-      shift
-      ;;
-    -nuclei_misconfig)
-      nuclei_misconfig=true
-      shift
-      ;;
-    -nuclei_takeovers)
-      nuclei_takeovers=true
-      shift
-      ;;
-    -nuclei_tech)
-      nuclei_tech=true
-      shift
-      ;;
-    -nuclei_vuln)
-      nuclei_vuln=true
-      shift
-      ;;
-    -cors)
-      cors=true
-      shift
-      ;;
-    -nmap)
-      nmap=true
-      shift
-      ;;
-    -crlf)
-      crlf=true
-      shift
-      ;;
-    -or)
-      or=true
-      shift
-      ;;
-    -output)
-      output=true
-      shift
-      ;;
-    *)
-      echo "${red}Argumento invalido: $key"
-      flags
-      exit 1
-      ;;
-  esac
+# Find cors
+notify "Searching cors"
+python3 $tools_dirs/Corsy/corsy.py -i $directory_data/$domain/httpx_info/alive_subdomains.txt -o $directory_data/$domain/vulns/cors.json
+
+# Find crlf
+notify "Search for CRLF"
+crlfuzz -l $directory_data/$domain/httpx_info/alive_subdomains.txt -o $directory_data/$domain/vulns/crlf.txt
+crlfuzz -l $directory_data/$domain/wayback_urls/urls.txt -o $directory_data/$domain/vulns/crlf2.txt
+cat $directory_data/$domain/vulns/crlf2.txt | anew $directory_data/$domain/vulns/crlf.txt
+rm $directory_data/$domain/vulns/crlf2.txt
+
+# Find secrets
+notify "Search secrets"
+cat $directory_data/$domain/secrets/katana.txt | grep "\.js$" | httpx -mc 200 -silent >> $directory_data/$domain/secrets/js_files.txt
+cat $directory_data/$domain/secrets/js_files.txt | while read url ; do
+	python3 $tools_dir/secretfinder/SecretFinder.py -i $url -o $directory_data/$domain/secrets/results.html
 done
 
-# Ejecutar tareas segun los flags
-if [ "$recon" = true ]; then
-
-  if [ -z "${domain}" ]; then
-   domain=${subreport[1]}
-   foldername=${subreport[2]}
-   subd=${subreport[3]}
-   report $domain $subdomain $foldername $subd; exit 1;
-   fi
-   clear
-   logo
-   if [ -d "$directory_data/$domain" ]
-   then
-     echo "${yellow}Este target fue escaneado previamente!."
-     exit
-   else
-     mkdir $directory_data/$domain
-fi
-
-todate=$(date +"%Y-%m-%d")
-path=$(pwd)
-foldername=scan-$todate
-  mkdir $directory_data/$domain/$foldername
-  mkdir $directory_data/$domain/$foldername/nmap
-
-##############################################################################Discovery START############################################################################
-  notify "El runner a iniciado el scan en $domain!"
-  echo "${green}Recon started in Subdomain $domain ${reset}"
-  echo "Listing subdomains using subfinder..."
-  subfinder -all -silent -d $domain -oI -nW > $directory_data/$domain/$foldername/subdomain_ip.csv
-  notify "Running amass"
-  amass enum -brute -d $domain | anew $directory_data/$domain/$foldername/subdomain_ip.csv
-  cat $directory_data/$domain/$foldername/subdomain_ip.csv | sed "s/[,].*//" | sort -u >> $directory_data/$domain/$foldername/$domain.txt
-  echo "${green}Probing for live hosts..."
-  echo $domain >> $directory_data/$domain/$foldername/$domain.txt
-  cat $directory_data/$domain/$foldername/$domain.txt | httpx -title -tech-detect -status-code -ip -p 66,80,81,443,445,457,1080,1100,1241,1352,1433,1434,1521,1944,2301,3000,3128,3306,4000,4001,4002,4100,5000,5432,5800,5801,5802,6082,6346,6347,7001,7002,8080,8443,8888,30821 >> $directory_data/$domain/$foldername/httpx_results.txt
-  cp $directory_data/$domain/$foldername/$domain.txt $directory_data/$domain/$foldername/subdomain.csv
-  cp $directory_data/$domain/$foldername/httpx.txt | awk '{print $1}' $directory_data/$domain/$foldername/urllist.csv
-  echo  "${yellow}Total of $(wc -l $directory_data/$domain/$foldername/urllist.csv | awk '{print $1}') live subdomains were found${reset}"
-fi
-
-
-
-##############################################################################wayback START############################################################################
-if [ "$wayback" = true ]; then
-#echo "${green}Starting to check available data in wayback machine"
-notify "Running GAU"
-waybackurls $domain > $directory_data/$domain/$foldername/wayback_tmp.txt
-gau -subs --threads 10 $domain >> $directory_data/$domain/$foldername/wayback_tmp.txt
-cat $directory_data/$domain/$foldername/wayback_tmp.txt | sort -u | uro > $directory_data/$domain/$foldername/wayback.txt
-rm $directory_data/$domain/$foldername/wayback_tmp.txt
-cat $directory_data/$domain/$foldername/wayback.txt | gf xss | kxss | grep -v "\[\]" >> $directory_data/$domain/$foldername/check_xss.txt
-notify "$(wc -l < $directory_data/$domain/$foldername/check_xss.txt) -> results to probe XSS"
-fi
-
-##############################################################################Dirsearch START############################################################################
-if [ "$dirsearch" = true ]; then
-echo "${green}Starting to check discovery with dirsearch"
-dirsearch -w ~/tools/SecLists/Discovery/Web-Content/directory-list-2.3-medium.txt -e $dirsearchExtensions -t 50 -exclude 403,401,404,400 -l $directory_data/$domain/$foldername/urllist.csv --deep-recursive -R 4 --crawl --full-url  --no-color --format=csv -o $directory_data/$domain/$foldername/dirsearch.csv
-fi
-
-##############################################################################OpenRedirect START############################################################################
-if [ "$or" = true ]; then
-notify "Starting to check Open Redirect"
-waybackurls $domain | grep -a -i \=http | qsreplace 'http://evil.com' | while read host do;do curl -s -L $host -I| echo -e "$host" ;done >> $directory_data/$domain/$foldername/openredirect.csv 2>/dev/null
-fi
-
-##############################################################################CORS START############################################################################
-if [ "$cors" = true ]; then
-notify "Starting to check CORS vulnerabilities"
-python3 $directory_tools/Corsy/corsy.py -i $directory_data/$domain/$foldername/urllist.csv -o $directory_data/$domain/$foldername/cors.json
-fi
-
-##############################################################################Port Scan START############################################################################
-if [ "$nmap" = true ]; then
-notify "Starting to check Open Ports"
-bash $directory_tools/customscripts/loop_nmap.sh $directory_data/$domain/$foldername/subdomain.csv
-mv nmap_full_* $directory_data/$domain/$foldername/nmap/
-fi
-
-##############################################################################CRLF START############################################################################
-if [ "$crlf" = true ]; then
-notify "Starting to check CRLF"
- crlfuzz -l $directory_data/$domain/$foldername/urllist.csv -o $directory_data/$domain/$foldername/crlfuzz_urllist.csv
- crlfuzz -l $directory_data/$domain/$foldername/wayback.txt -o $directory_data/$domain/$foldername/crlfuzz_wayback.txt
- cat $directory_data/$domain/$foldername/crlfuzz_urllist.csv > $directory_data/$domain/$foldername/crlfuzz.txt
- cat $directory_data/$domain/$foldername/crlfuzz_wayback.txt >> $directory_data/$domain/$foldername/crlfuzz.txt
- rm $directory_data/$domain/$foldername/crlfuzz_urllist.csv  $directory_data/$domain/$foldername/crlfuzz_wayback.txt
-fi
-
-##############################################################################nuclei START############################################################################
-if [ "$nuclei_cves" = true ]; then
-notify "Starting with nuclei"
-echo "{green}Starting to check cves"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t cves | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' > $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_dlogins" = true ]; then
-echo "{green}Starting to check default logins"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t default-logins | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_panels" = true ]; then
-echo "{green}Starting to check exposed panels"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t exposed-panels | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_exposures" = true ]; then
-echo "{green}Starting to check exposed information"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t exposures | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_misc" = true ]; then
-echo "{green}Starting to check miscellaneous"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t miscellaneous | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_misconfig" = true ]; then
-echo "{green}Starting to check misconfiguration"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t misconfiguration | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_takeovers" = true ]; then
-echo "{green}Starting to check DNS Takeovers"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t takeovers | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_tech" = true ]; then
-echo "{green}Starting to check technologies"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t technologies | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-if [ "$nuclei_vuln" = true ]; then
-notify "Nuclei has finished"
-echo "{green}Starting to check vulnerabilities"
-nuclei -l $directory_data/$domain/$foldername/urllist.csv -no-color -t vulnerabilities | sed 's/ /,/g; s/\[//g; s/\]//g; s/(//g; s/)//g' >> $directory_data/$domain/$foldername/nuclei.csv
-fi
-
-##############################################################################Output START############################################################################
-if [ "$output" = true ]; then
- scp -o  StrictHostKeyChecking=no -r ~/$domain $ssh_conection
- echo "{yellow}The recopiled data was moved to the Master node"
- zip -r "$domain.zip" $directory_data/$domain/$foldername
- notify "The scan has finished"
- notify "Your file.zip has been created -> $domain.zip"
+zip -r $directory_data/results_of_scan_$domain.zip $directory_data/$domain
+notify "The scan has finished"
