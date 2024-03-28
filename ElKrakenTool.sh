ed9103bdd5353337b8d750c2fb1bab681dc774b3
JMPESPX64 <110351733+JMPESPX64@users.noreply.github.com>
2024-03-28 13:45:07 +0100

    Update ElKrakenTool.sh

--- ElKrakenTool.sh
+++ ElKrakenTool.sh
@@ -34,27 +34,27 @@ function notify {
 }
 
 # Find subdomains 
-notify "Listing subdomains"
+notify "Listing subdomains on $domain"
 chaos -dL $domain -silent >> $directory_data/$domain/subdomains/subdomains.txt
 subfinder -dL $domain -all -silent | anew $directory_data/$domain/subdomains/subdomains.txt
 assetfinder --subs-only $domain | anew $directory_data/$domain/subdomains/subdomains.txt 
 
 # Get alive subdomains 
-notify "Running httpx"
+notify "Running httpx on $domain"
 cat $directory_data/$domain/subdomains/subdomains.txt | httpx -title -tech-detect -status-code -ip -p 80,443,8080,8081,9002,8443,81 >> $directory_data/$domain/httpx_info/httpx_full_info.txt
 
 cat $directory_data/$domain/httpx_info/httpx_full_info.txt | awk '{print $1}' >> $directory_data/$domain/httpx_info/alive_subdomains.txt
 
 # Take screenshots
-notify "Taking screenshots"
-cat $directory_data/$domain/httpx_info/alive_subdomains.txt | aquatone -out $directory_data/$domain/screenshots
+notify "Taking screenshots on $domain"
+cat $directory_data/$domain/httpx_info/alive_subdomains.txt | aquatone -out $directory_data/$domain/screenshots -chrome-path /snap/bin/chromium
 
 # Subdomains takeover 
-notify "Check subdomains takeover with subzy"
+notify "Check subdomains takeover with subzy on $domain"
 subzy run --targets $directory_data/$domain/httpx_info/alive_subdomains.txt --hide_fails >> $directory_data/$domain/vulns/posible_takeover.txt
 
 # XSS
-notify "Running gau"
+notify "Running gau on $domain"
 gau --threads 16 --subs --blacklist png,jpg,gif,svg,woff,woff2 $domain >> $directory_data/$domain/wayback_urls/urls.txt
 waybackurls $domain | anew $directory_data/$domain/wayback_urls/urls.txt
 notify "Running katana"
@@ -63,20 +63,21 @@ gf xss $directory_data/$domain/wayback_urls/urls.txt >> $directory_data/$domain/
 cat $directory_data/$domain/wayback_urls/parameter_urls.txt | uro | kxss | grep -v "\[\]" >> $directory_data/$domain/vulns/posible_xss.txt
 
 # Find cors
-notify "Searching cors"
+notify "Searching cors on $domain"
 python3 $tools_dirs/Corsy/corsy.py -i $directory_data/$domain/httpx_info/alive_subdomains.txt -o $directory_data/$domain/vulns/cors.json
 
 # Find crlf
-notify "Search for CRLF"
+notify "Search for CRLF on $domain"
 crlfuzz -l $directory_data/$domain/httpx_info/alive_subdomains.txt -o $directory_data/$domain/vulns/crlf.txt
 
 # Find secrets
-notify "Search secrets"
+notify "Search secrets on $domain"
 cat $directory_data/$domain/secrets/katana.txt >> $directory_data/$domain/secrets/js_files.txt
 cat $directory_data/$domain/secrets/js_files.txt | while read url ; do
 	python3 $tools_dir/secretfinder/SecretFinder.py -i $url -o $directory_data/$domain/secrets/results.html
 done
 
-zip -r results.zip $directory_data/$domain
+mkdir $directory_data/results
+cd $directory_data/results
+zip -r results_$domain.zip $directory_data/$domain
 notify "The scan has finished"
-notify "The ip adress is -> $(ip addr show eth0 | grep inet | awk 'NR==1' | cut -d '/' -f1 | tr -d 'inet ')"
